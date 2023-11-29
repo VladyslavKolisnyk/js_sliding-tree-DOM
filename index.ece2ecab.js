afcf064f973035d287fd7469c0c69e09142fac0e
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-11-29 12:17:27 +0200

    make build

---
+++ index.ece2ecab.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".tree");e.querySelectorAll("li").forEach(function(e){var t=document.createElement("span");t.className+="list-item",e.prepend(t),t.append(t.nextSibling)}),e.onclick=function(e){if("SPAN"===e.target.tagName){var t=e.target.parentNode.querySelector("ul");t&&(t.hidden=!t.hidden)}};//# sourceMappingURL=index.ece2ecab.js.map
+
+//# sourceMappingURL=index.ece2ecab.js.map
